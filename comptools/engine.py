8836a52b1f4f1bd3c10cd9695e4eecd6a4256ae0
AntJamGeo <57462383+AntJamGeo@users.noreply.github.com>
2023-01-19 15:53:29 +0000

    Make code more concise

--- comptools/engine.py
+++ comptools/engine.py
@@ -54,28 +54,16 @@ class CompilationEngine:
     def _compile_class(self):
         """
         Compiles code of the form:
-        'class' class_name '{' class_var_dec* subroutine_dec* '}'
+            'class' className '{' classVarDec* subroutineDec* '}'
         """
-
         self._open_block("class")
         self._assert_token("class")
-        self._write_terminal()
-
-        self._assert_token_type("identifier")
-        self._write_terminal()
-
+        self._assert_token_type("identifier") # className
         self._assert_token("{")
-        self._write_terminal()
-
         self._compile_class_var_dec()
-
         self._compile_subroutine()
-
         self._assert_token("}")
-        self._write_terminal()
-
         self._close_block("class")
-
         if self._tokenizer.has_more_tokens:
             raise JackError(
                 self._file_path,
@@ -87,125 +75,79 @@ class CompilationEngine:
     def _compile_class_var_dec(self):
         """
         Compiles code of the form:
-        ('static' | 'field') type var_name (',' var_name)* ';'
+            ('static' | 'field') type varName (',' varName)* ';'
         """
-
         while self._tokenizer.token in _CLASS_VAR_DEC_KEYWORDS:
             self._open_block("classVarDec")
             self._write_terminal()
-
             self._assert_type()
-            self._write_terminal()
-
-            self._assert_token_type("identifier")
-            self._write_terminal()
-
-            while self._tokenizer.token == ",":
-                self._write_terminal()
-
-                self._assert_token_type("identifier")
-                self._write_terminal()
-
+            self._assert_token_type("identifier") # varName
+            while self._check_token(","):
+                self._assert_token_type("identifier") # varName
             self._assert_token(";")
-            self._write_terminal()
-
             self._close_block("classVarDec")
 
     def _compile_subroutine(self):
         """
         Compiles code of the form:
-        ('constructor' | 'function' | 'method') ('void' | type)
-        subroutine_name '(' parameter_list ')' subroutine_body
+            ('constructor' | 'function' | 'method') ('void' | type)
+            subroutineName '(' parameterList ')' subroutineBody
         """
-
         while self._tokenizer.token in _SUBROUTINE_DEC_KEYWORDS:
             self._open_block("subroutineDec")
             self._write_terminal()
-
             self._assert_subroutine_type()
-            self._write_terminal()
-
-            self._assert_token_type("identifier")
-            self._write_terminal()
-
+            self._assert_token_type("identifier") # subroutineName
             self._assert_token("(")
-            self._write_terminal()
-
             self._compile_parameter_list()
-
             self._assert_token(")")
-            self._write_terminal()
-
+            # subroutineBody of form:
+                # '{' varDec* statements '}'
             self._open_block("subroutineBody")
-
             self._assert_token("{")
-            self._write_terminal()
-
             self._compile_var_dec()
-
             self._compile_statements()
-
             self._assert_token("}")
-            self._write_terminal()
-
             self._close_block("subroutineBody")
-
             self._close_block("subroutineDec")
 
     def _compile_parameter_list(self):
         """
         Compiles code of the form:
-        ( (type var_name) (',' type var_name)*)?
+            ( (type varName) (',' type varName)*)?
         """
 
         self._open_block("parameterList")
-
         if (self._tokenizer.token in _TYPE_KEYWORDS
                 or self._tokenizer.token_type == "identifier"):
             self._write_terminal()
-
-            self._assert_token_type("identifier")
-            self._write_terminal()
-
-            while self._tokenizer.token == ",":
-                self._write_terminal()
-
+            self._assert_token_type("identifier") # varName
+            while self._check_token(","):
                 self._assert_type()
-                self._write_terminal()
-
-                self._assert_token_type("identifier")
-                self._write_terminal()
-
+                self._assert_token_type("identifier") # varName
         self._close_block("parameterList")
 
     def _compile_var_dec(self):
         """
         Compiles code of the form:
-        'var' type var_name (',' var_name)* ';'
+            'var' type varName (',' varName)* ';'
         """
 
         while self._tokenizer.token == "var":
             self._open_block("varDec")
             self._write_terminal()
-
             self._assert_type()
-            self._write_terminal()
-
-            self._assert_token_type("identifier")
-            self._write_terminal()
-
-            while self._tokenizer.token == ",":
-                self._write_terminal()
-
-                self._assert_token_type("identifier")
-                self._write_terminal()
-
+            self._assert_token_type("identifier") # varName
+            while self._check_token(","):
+                self._assert_token_type("identifier") # varName
             self._assert_token(";")
-            self._write_terminal()
-
             self._close_block("varDec")
 
     def _compile_statements(self):
+        """
+        Compiles a series of statements, which must begin with the
+        keywords: 'let', 'if', 'while', 'do', or 'return'.
+        """
         self._open_block("statements")
         while True:
             if self._tokenizer.token == "let":
@@ -225,216 +167,145 @@ class CompilationEngine:
     def _compile_let(self):
         """
         Compile code of the form:
-        'let' var_name ('[' expression ']')? '=' expression ';'
+            'let' varName ('[' expression ']')? '=' expression ';'
         """
-
         self._open_block("letStatement")
         self._write_terminal()
-
-        self._assert_token_type("identifier")
-        self._write_terminal()
-
-        if self._tokenizer.token == "[":
-            self._write_terminal()
-
+        self._assert_token_type("identifier") # varName
+        if self._check_token("["):
             self._compile_expression()
-
             self._assert_token("]")
-            self._write_terminal()
-
         self._assert_token("=")
-        self._write_terminal()
-
         self._compile_expression()
-
         self._assert_token(";")
-        self._write_terminal()
-
         self._close_block("letStatement")
 
     def _compile_if(self):
         """
         Compile code of the form:
-        'if' '(' expression ')' '{' statements '}'
-        ('else' '{' statements '}')?
+            'if' '(' expression ')' '{' statements '}'
+            ('else' '{' statements '}')?
         """
         self._open_block("ifStatement")
         self._write_terminal()
-
         self._assert_token("(")
-        self._write_terminal()
-
         self._compile_expression()
-
         self._assert_token(")")
-        self._write_terminal()
-
         self._assert_token("{")
-        self._write_terminal()
-
         self._compile_statements()
-
         self._assert_token("}")
-        self._write_terminal()
-
-        if self._tokenizer.token == "else":
-            self._write_terminal()
-
+        if self._check_token("else"):
             self._assert_token("{")
-            self._write_terminal()
-
             self._compile_statements()
-
             self._assert_token("}")
-            self._write_terminal()
-
         self._close_block("ifStatement")
 
     def _compile_while(self):
         """
         Compile code of the form:
-        'while' '(' expression ')' '{' statements '}'
+            'while' '(' expression ')' '{' statements '}'
         """
         self._open_block("whileStatement")
         self._write_terminal()
-
         self._assert_token("(")
-        self._write_terminal()
-
         self._compile_expression()
-
         self._assert_token(")")
-        self._write_terminal()
-
         self._assert_token("{")
-        self._write_terminal()
-
         self._compile_statements()
-
         self._assert_token("}")
-        self._write_terminal()
-
         self._close_block("whileStatement")
 
     def _compile_do(self):
         """
         Compile code of the form:
-        'do' subroutine_call ';'
+            'do' subroutineCall ';'
         """
         self._open_block("doStatement")
         self._write_terminal()
-
-        self._assert_token_type("identifier")
-        self._write_terminal()
-
-        if self._tokenizer.token == ".":
-            self._write_terminal()
-
-            self._assert_token_type("identifier")
-            self._write_terminal()
-
+        # subroutineCall of form:
+            # ((className | varName) '.')?
+            # subroutineName '(' expressionList ')'
+        self._assert_token_type("identifier") # className | varName
+                                              # | subroutineName
+        if self._check_token("."):
+            self._assert_token_type("identifier") # subroutineName
         self._assert_token("(")
-        self._write_terminal()
-
         self._compile_expression_list()
-
         self._assert_token(")")
-        self._write_terminal()
-
         self._assert_token(";")
-        self._write_terminal()
-
         self._close_block("doStatement")
 
     def _compile_return(self):
         """
         Compile code of the form:
-        'return' expression? ';'
+            'return' expression? ';'
         """
         self._open_block("returnStatement")
         self._write_terminal()
-
         if self._tokenizer.token != ";":
             self._compile_expression()
-
         self._assert_token(";")
-        self._write_terminal()
-
         self._close_block("returnStatement")
 
     def _compile_expression(self):
+        """
+        Compile code of the form:
+            term (op term)*
+        """
         self._open_block("expression")
-
         self._compile_term()
-
-        while self._tokenizer.token in _OPS:
-            self._write_terminal()
+        while self._check_token(_OPS):
             self._compile_term()
-
         self._close_block("expression")
 
     def _compile_term(self):
+        """
+        Compile a term, which can be one of the following:
+            - integerConstant
+            - stringConstant
+            - keywordConstant
+            - varName
+            - varName '[' expression ']'
+            - subroutineCall
+            - '(' expression ')'
+            - unaryOp term
+        """
         self._open_block("term")
-
         if (self._tokenizer.token_type == "integerConstant"
                 or self._tokenizer.token_type == "stringConstant"
                 or self._tokenizer.token in _KEYWORD_CONSTANTS):
             self._write_terminal()
         elif self._tokenizer.token_type == "identifier":
             self._write_terminal()
-            if self._tokenizer.token == "[":
-                self._write_terminal()
-
+            if self._check_token("["):
                 self._compile_expression()
-
                 self._assert_token("]")
-                self._write_terminal()
-            elif self._tokenizer.token == "(":
-                self._write_terminal()
-
+            elif self._check_token("("):
                 self._compile_expression_list()
-
                 self._assert_token(")")
-                self._write_terminal()
-            elif self._tokenizer.token == ".":
-                self._write_terminal()
-
+            elif self._check_token("."):
                 self._assert_token_type("identifier")
-                self._write_terminal()
-
                 self._assert_token("(")
-                self._write_terminal()
-
                 self._compile_expression_list()
-
                 self._assert_token(")")
-                self._write_terminal()
-        elif self._tokenizer.token == "(":
-            self._write_terminal()
-
+        elif self._check_token("("):
             self._compile_expression()
-
             self._assert_token(")")
-            self._write_terminal()
-        elif self._tokenizer.token in _UNARY_OPS:
-            self._write_terminal()
-
+        elif self._check_token(_UNARY_OPS):
             self._compile_term()
-
         self._close_block("term")
 
     def _compile_expression_list(self):
+        """
+        Compile code of the form:
+            (expression (',' expression)*)?
+        """
         self._open_block("expressionList")
-
         if (self._tokenizer.token in _TYPE_TOKENS
                 or self._tokenizer.token_type in _TYPE_TYPES):
             self._compile_expression()
-
-            while self._tokenizer.token == ",":
-                self._write_terminal()
-
+            while self._check_token(","):
                 self._compile_expression()
-
         self._close_block("expressionList")
 
     # -----------------------WRITING FUNCTIONS-----------------------
@@ -470,6 +341,7 @@ class CompilationEngine:
                 f"On line {self._tokenizer.line_no}, "
                 f"expected token '{token}' but got '{self._tokenizer.token}'."
             )
+        self._write_terminal()
 
     def _assert_token_type(self, type_):
         self._assert_has_more_tokens()
@@ -481,6 +353,7 @@ class CompilationEngine:
                 f"'{self._tokenizer.token}' which is a "
                 f"'{self._tokenizer.token_type}'."
             )
+        self._write_terminal()
 
     def _assert_type(self):
         self._assert_has_more_tokens()
@@ -492,6 +365,7 @@ class CompilationEngine:
                 "expected a type (int, char, boolean, or class name) but got "
                 f"'{self._tokenizer.token}'."
             )
+        self._write_terminal()
 
     def _assert_subroutine_type(self):
         self._assert_has_more_tokens()
@@ -503,6 +377,14 @@ class CompilationEngine:
                 "expected a return type (void, int, char, boolean, or class "
                 f"name) but got '{self._tokenizer.token}'."
             )
+        self._write_terminal()
+
+    # ----------------------CHECKING FUNCTIONS-----------------------
+    def _check_token(self, token):
+        if self._tokenizer.token in token:
+            self._write_terminal()
+            return True
+        return False
 
 
 _INDENT_SPACES = 2
